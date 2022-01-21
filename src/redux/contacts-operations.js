0b9bc71fd6fe3ec5319f82f71a9aa47c2ebce95a
Ivan Suprun <bender.slam@gmail.com>
2022-01-22 00:45:17 +0200

    bugfix

--- src/redux/contacts-operations.js
+++ src/redux/contacts-operations.js
@@ -1,33 +1,50 @@
 // Imports of actions
-import { createAsyncThunk } from '@reduxjs/toolkit';
-  // Imports of libraries
-  import axios from 'axios';
-  axios.defaults.baseURL = 'https://61eb18a77ec58900177cdbb5.mockapi.io';
-  
-  export const fetchContacts = createAsyncThunk(
-    'contacts/fetchContacts',
-    async () => {
-      const { data } = await axios.get('/contacts');
-      return data;
-    }
-  );
-  
-  export const deleteContact = createAsyncThunk(
-    'contacts/deleteContact',
-    async contactId => {
-      const {
-        data: { id },
-      } = await axios.delete(`/contacts/${contactId}`);
-      return id;
-    }
-  );
-  
-  export const addContact = createAsyncThunk(
-    'contacts/addContact',
-    async ({ name, number: phone }) => {
-      const contact = { name, phone };
-  
-      const { data } = await axios.post('/contacts', contact);
-      return data;
-    }
-  );
+import {
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+} from './contacts-actions';
+// Imports of libraries
+import axios from 'axios';
+axios.defaults.baseURL = 'https://61eb18a77ec58900177cdbb5.mockapi.io';
+
+const fetchContacts = () => async dispatch => {
+  dispatch(fetchContactsRequest());
+
+  try {
+    const { data } = await axios.get('/contacts');
+    dispatch(fetchContactsSuccess(data));
+  } catch (error) {
+    dispatch(fetchContactsError(error));
+  }
+};
+
+const addContact = contact => async dispatch => {
+  dispatch(addContactRequest());
+
+  try {
+    const { data } = await axios.post('/contacts', contact);
+    dispatch(addContactSuccess(data));
+  } catch (error) {
+    dispatch(addContactError(error));
+  }
+};
+
+const deleteContact = contactId => async dispatch => {
+  dispatch(deleteContactRequest());
+
+  try {
+    await axios.delete(`/contacts/${contactId}`);
+    dispatch(deleteContactSuccess(contactId));
+  } catch (error) {
+    dispatch(deleteContactError(error));
+  }
+};
+
+export { fetchContacts, addContact, deleteContact };
